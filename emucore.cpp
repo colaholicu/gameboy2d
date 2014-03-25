282db08270d33e642e26bbb4c7d3fb0aa2426f22
colaholicu <->
2014-03-25 23:12:16 +0200

    added member variables to the emulator
    added screen
    added registers
    added memory
    added decode(), ZeroMem(), Cycle() & Draw()

--- emucore.cpp
+++ emucore.cpp
@@ -4,9 +4,28 @@
 
 Gameboy2d::Gameboy2d(void)
 {
+    ZeroMem();
 }
 
 
 Gameboy2d::~Gameboy2d(void)
+{
+}
+
+
+void Gameboy2d::ZeroMem()
+{
+    ZeroMemory(memory, 0xffff);
+    ZeroMemory(screen, SCREEN_SIZE);
+
+    AF.val = BC.val = DE.val = HL.val = PC = SP = 0;
+    IR = RR = 0;
+}
+
+void Gameboy2d::Cycle()
+{
+}
+
+void Gameboy2d::Draw()
 {
 }
--- emucore.h
+++ emucore.h
@@ -1,7 +1,53 @@
 #pragma once
+
+#include "typedefs.h"
+
+#define SCREEN_WIDTH    (160)
+#define SCREEN_HEIGHT   (144)
+#define SCREEN_SIZE     (160 * 144)
+
+#define Z   ((AF.u.lo) & 0x80) // Zero flag
+#define C   ((AF.u.lo) & 0x10) // Carry flag
+
 class Gameboy2d
 {
+private:    
+    reg AF, BC, DE, HL; // registers
+
+    u8  IR; // interrupt register
+    u8  RR; // refresh register
+
+    u16 PC; // program counter
+    u16 SP; // stack pointer
+
+    u16 opcode; // opcode
+
+    /*___________________________MEMORY_MAP__________________________________
+    0000-3FFF   16KB ROM Bank 00     (in cartridge, fixed at bank 00)
+    4000-7FFF   16KB ROM Bank 01..NN (in cartridge, switchable bank number)
+    8000-9FFF   8KB Video RAM (VRAM) (switchable bank 0-1 in CGB Mode)
+    A000-BFFF   8KB External RAM     (in cartridge, switchable bank, if any)
+    C000-CFFF   4KB Work RAM Bank 0 (WRAM)
+    D000-DFFF   4KB Work RAM Bank 1 (WRAM)  (switchable bank 1-7 in CGB Mode)
+    E000-FDFF   Same as C000-DDFF (ECHO)    (typically not used)
+    FE00-FE9F   Sprite Attribute Table (OAM)
+    FEA0-FEFF   Not Usable
+    FF00-FF7F   I/O Ports
+    FF80-FFFE   High RAM (HRAM)
+    FFFF        Interrupt Enable Register */
+    u8  memory[0xffff];
+
+    u8  screen[SCREEN_SIZE];
+
+protected:
+
+    void decode();
+    void ZeroMem();
+
 public:
     Gameboy2d(void);
     virtual ~Gameboy2d(void);
+
+    void Cycle();
+    void Draw();
 };
