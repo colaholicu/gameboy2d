879e090d79b1605d176297fdbb96a28a3a841a19
colaholicu <->
2014-03-25 21:01:28 +0200

    core emulator files initial commit

---
+++ emucore.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "emucore.h"
+
+
+gameboy2d::gameboy2d(void)
+{
+}
+
+
+gameboy2d::~gameboy2d(void)
+{
+}
---
+++ emucore.h
@@ -0,0 +1,8 @@
+#pragma once
+class gameboy2d
+{
+public:
+    gameboy2d(void);
+    virtual ~gameboy2d(void);
+};
+
